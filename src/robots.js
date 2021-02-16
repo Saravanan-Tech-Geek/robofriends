1dcb903eb657a9c8b4dc5eaab0990b8b1b2250ac
Saravanan-Tech-Geek <sarobioinfo@gmaile.com>
2021-02-16 16:01:04 +0530

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'bharani',
+        username: 'bhar',
+        email: 'bharani@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'saravanan',
+        username: 'sar',
+        email: 'bharani@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'jaga',
+        username: 'jag',
+        email: 'bharani@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'suresh',
+        username: 'bsusthar',
+        email: 'bharani@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'sham',
+        username: 'shu',
+        email: 'bharani@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'gokularani',
+        username: 'bhar',
+        email: 'bharani@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'bharani',
+        username: 'bhar',
+        email: 'bharani@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'bharani',
+        username: 'bhar',
+        email: 'bharani@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'bharani',
+        username: 'bhar',
+        email: 'bharani@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'bharani',
+        username: 'bhar',
+        email: 'bharani@gmail.com'
+    }
+]
